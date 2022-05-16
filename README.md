6cb9af7e4cee9b1527fb26effb9be3fe7e1ef012
Mehmet Cinar <mcinarpro@gmail.com>
2022-05-16 18:22:37 +0200

    feat: add tailwindcss

---
+++ README.md
@@ -0,0 +1,7 @@
+## Installing Mcinar UI
+
+To use Mcinar UI components in React, all you need to do is install the `mcinar-ui` package.
+
+```sh
+$ npm i mcinar-ui
+```
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -5,7 +5,11 @@ interface ButtonProps {
 }
 
 const Button = (props: ButtonProps) => {
-  return <button>{props.label}</button>;
+  return (
+    <button className="inline-flex items-center rounded px-4 py-2 font-semibold focus:outline-none focus-visible:ring focus-visible:ring-primary-500 shadow-sm bg-primary-500 text-white border border-primary-600 hover:bg-primary-600 hover:text-white active:bg-primary-500 disabled:bg-primary-400 disabled:hover:bg-primary-400">
+      {props.label}
+    </button>
+  );
 };
 
 export default Button;
