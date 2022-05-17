90f06c61b3e8feeb9ae734947311f8293eddf535
Mehmet Cinar <mcinarpro@gmail.com>
2022-05-18 00:47:18 +0200

    feat: add withTheme

---
+++ src/utils/withTheme.ts
@@ -0,0 +1,36 @@
+const merge = require("deepmerge");
+const colors = require("../theme/base/colors");
+// const typography = require("../theme/base/typography");
+// const shadows = require("../theme/base/shadows");
+// const breakpoints = require("../theme/base/breakpoints");
+
+const themeConfig = {
+  darkMode: "class",
+  content: [
+    "./node_modules/@mcinarpro/mcinar-ui/components/**/*.{js,ts,jsx,tsx}",
+    // "./node_modules/@mcinarpro/mcinar-ui/theme/components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors,
+    // fontFamily: typography,
+    // boxShadow: shadows,
+    // screens: breakpoints,
+  },
+  plugins: [],
+};
+
+function withTheme(tailwindConfig : any) {
+  // const themeFont = themeConfig.theme.fontFamily;
+
+  // if (themeConfig.theme.fontFamily) {
+  //   const { sans, serif, body } = tailwindConfig.theme.fontFamily;
+
+  //   themeFont.sans = sans || themeFont.sans;
+  //   themeFont.serif = serif || themeFont.serif;
+  //   themeFont.body = body || themeFont.body;
+  // }
+
+  return merge(themeConfig, { ...tailwindConfig });
+}
+
+module.exports = withTheme;
